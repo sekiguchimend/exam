d42d318cc4e55756ae6e21e73569025a2bca51d5
関口峻矢 <sekiguchishunya0619@outlook.jp>
2024-03-23 00:16:53 +0900

    🐛

--- app/api/page.tsx
+++ app/api/page.tsx
@@ -1,7 +1,6 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export const GET = (request: NextRequest) => {
+export default function handler(request: NextRequest) {
   // リクエストの処理など
 
   // エラー処理
