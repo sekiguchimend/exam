e9f2ab8205c1eb968bc8ebd9acb14b834f1a3f88
関口峻矢 <sekiguchishunya0619@outlook.jp>
2024-03-23 00:04:59 +0900

    🔥

--- app/api/page.tsx
+++ app/api/page.tsx
@@ -1,13 +1,12 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextApiRequest, NextApiResponse } from "next";
 
-export const GET = (request: NextRequest) => {
+export default function sendGmail(req: NextApiRequest, res: NextApiResponse) {
   // リクエストの処理など
 
   // エラー処理
-  if (!request) {
-    return NextResponse.json({ error: 'Request object is not defined' }, { status: 500 })
+  if (!req) {
+    return res.status(500).json({ error: "Request object is not defined" });
   }
 
-  return NextResponse.json({ name: 'shunya' })
+  return res.status(200).json({ name: "shunya" });
 }
