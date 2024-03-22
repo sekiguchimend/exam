93ea83250a88fc92824c336d0c1cbb6d09c383c0
関口峻矢 <sekiguchishunya0619@outlook.jp>
2024-03-23 00:28:46 +0900

    🐛

---
+++ pages/api/myapi.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  // リクエストの処理など
+
+  // エラー処理
+  if (!req) {
+    return res.status(500).json({ error: 'Request object is not defined' })
+  }
+
+  return res.status(200).json({ name: 'shunya' })
+}
